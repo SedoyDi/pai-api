6527b213462774464f53b113103cfd2f1d54fe53
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2024-02-10 17:40:29 +0300

    оптимизация - убрал лишний цикл

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ let fs = require("fs");
 
 let inputFileContent = fs.readFileSync("./input/чеки.txt", "utf8");
 let outputFileContent = "./output/чеки_по_папкам.txt";
-let cheques = inputFileContent.replaceAll(".pdf", "").split("\r\n");
 
 const paid = {};
 const moths = [];
-const services = Array.from(new Set(cheques.map((c) => {
-    return c.replace(/\_.*/, '')
-})));
+let cheques = [];
+let services = [];
+let outCheques = [];
 
 function writeToOutFile(text) {
     try {
@@ -18,15 +17,28 @@ function writeToOutFile(text) {
     }
 }
 
-const outCheques = cheques.map((c) => {
+cheques = inputFileContent.replaceAll(".pdf", "").split("\r\n");
+
+outCheques = cheques.map((c) => {
     let newCheque = ''
-    const ch = c.split("_");
-    paid[ch[1]] = paid[ch[1]] ? [...paid[ch[1]], ch[0]] : [ch[0]];
-    moths.push(ch[1]);
-    newCheque = `/ ${ch[1]} / ${ch[0]}_${ch[1]}.pdf`
-    return newCheque
+    const cheque = c.split("_");
+    const service = c.replace(/\_.*/, '')
+
+    services = [...services, service];
+
+    paid[cheque[1]] = paid[cheque[1]]
+        ? [...paid[cheque[1]], cheque[0]]
+        : [cheque[0]];
+
+    moths.push(cheque[1]);
+
+    newCheque = `/ ${cheque[1]} / ${cheque[0]}_${cheque[1]}.pdf`
+
+    return newCheque;
+
 }).sort((a, b) => a.localeCompare(b));
 
+services = Array.from(new Set(services));
 
 outCheques.forEach((c) => writeToOutFile(`${c}\n`));
 
@@ -34,7 +46,6 @@ writeToOutFile("Не оплачены:");
 
 for (const moth of new Set(moths)) {
     const s = new Set(paid[moth]);
-
     const filredServices = services.filter((e) => !s.has(e));
 
     if (filredServices.length === 0) {
