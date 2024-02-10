3ab3a745db5c46ce6b0f636b13c0bc103a30eb8c
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2024-02-10 17:58:16 +0300

    оптимизация 2.0 - заменил цикл for на метод forEach

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let inputFileContent = fs.readFileSync("./input/чеки.txt", "utf8");
 let outputFileContent = "./output/чеки_по_папкам.txt";
 
 const paid = {};
-const moths = [];
+let months = [];
 let cheques = [];
 let services = [];
 let outCheques = [];
@@ -21,6 +21,7 @@ cheques = inputFileContent.replaceAll(".pdf", "").split("\r\n");
 
 outCheques = cheques.map((c) => {
     let newCheque = ''
+
     const cheque = c.split("_");
     const service = c.replace(/\_.*/, '')
 
@@ -30,7 +31,7 @@ outCheques = cheques.map((c) => {
         ? [...paid[cheque[1]], cheque[0]]
         : [cheque[0]];
 
-    moths.push(cheque[1]);
+    months.push(cheque[1]);
 
     newCheque = `/ ${cheque[1]} / ${cheque[0]}_${cheque[1]}.pdf`
 
@@ -38,21 +39,20 @@ outCheques = cheques.map((c) => {
 
 }).sort((a, b) => a.localeCompare(b));
 
+months = Array.from(new Set(months));
+
 services = Array.from(new Set(services));
 
 outCheques.forEach((c) => writeToOutFile(`${c}\n`));
 
 writeToOutFile("Не оплачены:");
 
-for (const moth of new Set(moths)) {
-    const s = new Set(paid[moth]);
+months.forEach((month) => {
+    const s = new Set(paid[month]);
     const filredServices = services.filter((e) => !s.has(e));
 
-    if (filredServices.length === 0) {
-        continue;
+    if (filredServices.length !== 0) {
+        writeToOutFile(`\n${month}: \n`);
+        writeToOutFile(filredServices.join("\n"));
     }
-
-    writeToOutFile(`\n${moth}:\n`);
-    writeToOutFile(filredServices.join("\n"));
-};
-
+});
